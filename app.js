7792e2ef24b05fb77545575294597818bf976d15
kim_reulgar <82751941+kimregular@users.noreply.github.com>
2021-12-22 10:06:50 +0900

    project started

---
+++ app.js
@@ -0,0 +1,11 @@
+const a = 5;
+const b = 3;
+const myName = "jk";
+
+console.log("fuck you!");
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+alert("fuck you!");
