f177e3a3d0fa5455baaf813ee648ff56b6912410
RegularKim <RegularKim@DESKTOP-M8B374L.localdomain>
2021-12-22 15:31:51 +0900

    till working on

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const calc = {
-  plus: function (a, b) {
-    console.log(a + b);
-  },
-  minus: function (a, b) {
-    console.log(a - b);
-  },
-  times: function (a, b) {
-    console.log(a * b);
-  },
-  divide: function (a, b) {
-    console.log(a / b);
-  },
-};
-calc.divide(10, 12);
+const age = parseInt(prompt("How Old Are You?"));
+
+function drinkAge(age) {
+  if (isNaN(age) || age <= 0) {
+    return "Plz Enter The Right Value";
+  } else if (age < 18) {
+    return "How Dare You?";
+  } else if (age >= 18 && age < 50) {
+    return "Enjoy Your Drink";
+  } else {
+    return "You Should Take Care Of Your Own Good";
+  }
+}
+
+console.log(drinkAge(age));
