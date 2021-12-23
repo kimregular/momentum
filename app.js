3d645bb5db7744b467030746dbdf691433ac71d2
Regular_Kim <kim_regular@icloud.com>
2021-12-24 05:51:31 +0900

    working on_pc

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-const a = 5;
-const b = 3;
-const myName = "jk";
+const title = document.getElementById("title");
 
-console.log("fuck you!");
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
-
-alert("fuck you!");
+console.log(title);
